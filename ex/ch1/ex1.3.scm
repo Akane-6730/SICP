123842fe28ff8111afdf230241a46620e733bd56
Akane <710105188@qq.com>
2025-07-09 20:01:13 +0800

    Add Org file to track exercise solutions of chapter1

--- ex/ch1/ex1.3.scm
+++ ex/ch1/ex1.3.scm
@@ -1,34 +1,31 @@
-;;EXCERCISE 1.3
+;; EXERCISE 1.3
 
 ;; square
-(define (square x) (* x x))
+(define (square x)
+  (* x x))
 
-;; sum-squares-of-3
-(define (sum-squares-of-3 x y z)
-  (+ (square x)
-     (square y)
-     (square z)))
-
-;; min
-(define (min x y)
-  (if (< x y)
-      x
-      y))
-
-;; min-of-3
-(define (min-of-3 x y z)
-  (min x (min y z)))
+;; sum-of-squares
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
 
 ;; sum-squares-of-two-largest
 (define (sum-squares-of-two-largest x y z)
-  (- (sum-squares-of-3 x y z)
-     (square (min-of-3 x y z))))
+  (cond ((and (<= x y) (<= x z)) (sum-of-squares y z)) ; Case 1: x is the smallest
+        ((<= y z) (sum-of-squares x z))                ; Case 2: x is not the smallest, and z is larger than y
+        (else (sum-of-squares x y))))                  ; Case 3: x is not the smallest, and y is larger than z
 
 ;; some test cases
-(sum-squares-of-two-largest 1 2 3)
-(sum-squares-of-two-largest 2 1 3)
-(sum-squares-of-two-largest 3 2 1)
-(sum-squares-of-two-largest 5 5 5)
-(sum-squares-of-two-largest -1 -2 -3)
-(sum-squares-of-two-largest 0 0 0)
-(sum-squares-of-two-largest 3 5 7)
+;; Test case 1: Numbers are in increasing order
+(sum-squares-of-two-largest 1 2 3) ; Expected: 13 (2*2 + 3*3)
+
+;; Test case 2: Numbers are in decreasing order
+(sum-squares-of-two-largest 3 2 1) ; Expected: 13 (3*3 + 2*2)
+
+;; Test case 3: Smallest number is in the middle
+(sum-squares-of-two-largest 3 1 2) ; Expected: 13 (3*3 + 2*2)
+
+;; Test case 4: Two numbers are equal and are the largest
+(sum-squares-of-two-largest 2 4 4) ; Expected: 32 (4*4 + 4*4)
+
+;; Test case 5: All numbers are equal
+(sum-squares-of-two-largest 5 5 5) ; Expected: 50 (5*5 + 5*5)
